ff2f45c970bfb68462cb126060041bebb4076878
Praphulla06 <praphullamalla62@gmail.com>
2024-05-27 22:11:05 +0545

    function

---
+++ function/function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int add (int a, int b) // Prototyping (Declaration) of a function.
+{
+    // Defining of a function.
+    int c;
+    c = a + b;
+    return c; 
+}
+
+int main()
+{
+    int num1 = 10, num2 = 15, sum;
+    sum = add(num1, num2); // Calling a function
+    printf("Sum: %d", sum);
+    return 0;
+}

7ad25e69e8e934aa96b0bf4ca2ca69f5547c6f21
Praphulla06 <praphullapradhananga815@gmail.com>
2024-06-19 17:03:20 +0545

    Sum of n Natural numbers

--- index.c
+++ index.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
-int fun(int n)
-{
-    if (n > 100)
-    {
-        return n - 10;
-    }
-    else
-    {
-        return fun(fun(n+11));
+void printChar(char *str, int index) {
+    if (str[index] == '\0') return;
+    putchar(str[index]);
+    printChar(str, index + 1);
+}
+
+void printNested(char *str, int outer, int inner) {
+    if (outer == 0) return;
+    if (inner > 0) {
+        printNested(str, outer, inner - 1);
+    } else {
+        printChar(str, 0);
+        putchar('\n');
+        printNested(str, outer - 1, outer - 1);
     }
 }
 
-int main()
-{
-    int x = 95;
-    printf("%d ", fun(x)); 
+int main() {
+    char str[] = "Waking up every day feels like living inside a nested recursion!";
+    printNested(str, 1, 1);
+    return 0;
 }
